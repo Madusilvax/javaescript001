2e94d59ee18338a2d958d09729212aa95eb99ab2
Maria Eduarda <ma.eferreira20@gmail.com>
2025-05-06 22:18:03 -0300

    console

--- app01.js
+++ app01.js
@@ -3,9 +3,16 @@
 // Sintaxe básica e comentário
 // Declaração de variáveis(var, let, const)
 let nome = "maria"
-let idade =21 
+let idade =21
 
 // Console e Debug
+console.log(nome)
+console.log(idade)
+nome="maria"
+idade= 21
+console.log(nome, idade)
+
+
 
 // Operadores
 //     Operador Atribuição
