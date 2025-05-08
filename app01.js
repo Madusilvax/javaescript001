ab6d3ef11bf33497957c7367ae56c4fab1df55cc
Maria Eduarda <ma.eferreira20@gmail.com>
2025-05-08 20:17:41 -0300

    operadores

--- app01.js
+++ app01.js
@@ -28,6 +28,39 @@ console.log(typeof cidade)
 console.log(typeof salario)
 console.log(typeof fumante)
 
+//operadores
+// operador atribuição(=)
+// operador aritimético(+, -, /, *)
+console.log("10+15")
+let n1= 10
+let n2= 5
+console.log(n1+n2)// soma
+console.log(n1-n2)// subtração
+console.log(n1/n2)// divisão
+console.log(n1*n2)// multiplicação
+console.log(n1%3)// modulo (resto da divisão)
+
+
+
+// operadores relacionais (>, <, >=, <=, !=, ==)
+console.log(n1==n2)// iguadade
+console.log(n1!=n2)// diferente
+console.log(n1>n2)// maior
+console.log(10<10)// menor igual
+console.log(150 >= 175)// maior igual
+
+
+// operadores logicos (!, && ,||)
+console.log(!10>2)// não- inverte o resultado, se for verdadeiro muda para falso, se for falço muda pra verdadeiro
+console.log(!false)
+
+console.log(10>2 && 35<100 &&n1>n2 && 100<90)// todas as verificações precisam ser verdadeiras para o resultado ser verdadeiro
+
+console.log(10<2 ||100==150 || 57==57)// OU- apenas uma verificação precisa ser verdadeira para o resultado ser verdadeiro, o resultado so sera falso
+// quando TODAS as verificações forem falsas 
+
+
+
 
 
 
